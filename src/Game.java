7ed580e3568eedeb4ce91b4a9e9559b100de5fe8
Jeky-v <jeky-v@mail.ru>
2013-08-09 14:24:58 +0300

    Changed main game loop.

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,77 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 
 public class Game
 {
     private static final String ERROR_MASSAGE = "Incorrect Input !";
     private static final int DEFAULT_NCELLS_TO_WIN = 3;
-    private static Scanner input = new Scanner(System.in);
-    private static int numberCellToWin;
+    private static final int DEFAULT_FIELD_SIZE = 3;
+    private static int numberCellsToWin;
     private Field field;
     private Player player1;
     private Player player2;
     private ArrayList<Stroke> history;
 
-    private void gameLoop()
+    public Game()
     {
+        this(new HumanPlayer(), new HumanPlayer(), DEFAULT_FIELD_SIZE , DEFAULT_NCELLS_TO_WIN);
+    }
+    public Game(Player newPlayer1, Player newPlayer2,int newFieldSize, int newNumberCellsToWin)
+    {
+        if(newPlayer1 != null && newPlayer2 != null)
+        {
+            player1 = newPlayer1;
+            player2 = newPlayer2;
+        }
+        else
+        {
+            System.out.print("Game constructor error");
+        }
+        field = new Field(newFieldSize);
+        field.eraseField();
+        history = new ArrayList<Stroke>();
+        numberCellsToWin = newNumberCellsToWin;
+    }
+    private boolean checkWin()
+    {
+        Stroke stroke = field.getLastStroke();
+        int x = stroke.x;
+        int y = stroke.y;
+        int size = field.getFieldSize();
+        int i,j;
+        //checking of lines
+        for(i = y; i >= 0 && field.getField()[x][i] == stroke.gameChar; i--);
+        for(j = y; j < size && field.getField()[x][j] == stroke.gameChar; j++);
+        if(j - i - 1 >= numberCellsToWin)
+        {
+            return true;
+        }
+        //checking of columns
+        for(i = x; i >= 0 && field.getField()[i][y] == stroke.gameChar; i--);
+        for(j = x; j < size && field.getField()[j][y] == stroke.gameChar; j++);
+        if(j - i - 1 >= numberCellsToWin)
+        {
+            return true;
+        }
+        //checking of primary diagonals
+        for(i = 1; x - i >= 0 && y - i >= 0 && field.getField()[x - i][y - i] == stroke.gameChar; i++);
+        for(j = 1; x + j < size && y + j < size && field.getField()[x + j][y + j] == stroke.gameChar; j++);
+        if(j + i - 1 >= numberCellsToWin)
+        {
+            return true;
+        }
+        //checking of secondary diagonal
+        for(i = 1; x + i < size && y - i >= 0 && field.getField()[x + i][y - i] == stroke.gameChar; i++);
+        for(j = 1; x - j >=0 && y + j < size && field.getField()[x - j][y + j] == stroke.gameChar; j++);
+        if(j + i - 1 >= numberCellsToWin)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void startGame()
+    {
+        field.showField();
         boolean firstPlayer = true;
         Player currentPlayer;
         while(field.isAnyEmptyCells())
@@ -42,102 +99,6 @@ public class Game
         }
         field.showField();
         System.out.println("DRAW !");
-    }
-    public Game()
-    {
-        numberCellToWin = DEFAULT_NCELLS_TO_WIN;
-    }
-    private boolean checkWin()
-    {
-        Stroke stroke = field.getLastStroke();
-        int x = stroke.x;
-        int y = stroke.y;
-        int size = field.getFieldSize();
-        int i,j;
-        //checking of lines
-        for(i = y; i >= 0 && field.getField()[x][i] == stroke.gameChar; i--);
-        for(j = y; j < size && field.getField()[x][j] == stroke.gameChar; j++);
-        if(j - i - 1 >= numberCellToWin)
-        {
-            return true;
-        }
-        //checking of columns
-        for(i = x; i >= 0 && field.getField()[i][y] == stroke.gameChar; i--);
-        for(j = x; j < size && field.getField()[j][y] == stroke.gameChar; j++);
-        if(j - i - 1 >= numberCellToWin)
-        {
-            return true;
-        }
-        //checking of primary diagonals
-        for(i = 1; x - i >= 0 && y - i >= 0 && field.getField()[x - i][y - i] == stroke.gameChar; i++);
-        for(j = 1; x + j < size && y + j < size && field.getField()[x + j][y + j] == stroke.gameChar; j++);
-        if(j + i - 1 >= numberCellToWin)
-        {
-            return true;
-        }
-        //checking of secondary diagonal
-        for(i = 1; x + i < size && y - i >= 0 && field.getField()[x + i][y - i] == stroke.gameChar; i++);
-        for(j = 1; x - j >=0 && y + j < size && field.getField()[x - j][y + j] == stroke.gameChar; j++);
-        if(j + i - 1 >= numberCellToWin)
-        {
-            return true;
-        }
-        return false;
-    }
-    public void startGame()
-    {
-        boolean run = true;
-
-        System.out.println("Hello! It's Tic Tac Toe Game!");
-        System.out.println("Please, choose one of the next game modes:");
-        System.out.println("1. Human vs Human");
-        System.out.println("2. Human vs Computer");
-        System.out.println("3. Exit");
-        System.out.print("I'm choosing: ");
-
-        while(run)
-        {
-            if(input.hasNextInt())
-            {
-                switch(input.nextInt())
-                {
-                    case 1:
-                    {
-                        player1 = new HumanPlayer();
-                        player2 = new HumanPlayer();
-                        run = false;
-                        break;
-                    }
-                    case 2:
-                    {
-                        player1 = new HumanPlayer();
-                        player2 = new ComputerPlayer();
-                        run = false;
-                        break;
-                    }
-                    case 3:
-                    {
-                        run = false;
-                        return;
-                    }
-                    default:
-                    {
-                        System.out.println(ERROR_MASSAGE);
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                System.out.println(ERROR_MASSAGE);
-            }
-        }
-        field = new Field();
-        field.eraseField();
-        System.out.println("The Game started !");
-        field.showField();
-        history = new ArrayList<Stroke>();
-        gameLoop();
     }
     public void showHistory()
     {
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,70 @@
+import java.util.Scanner;
+
 public class Main
 {
+    private static final String ERROR_MASSAGE = "Incorrect Input !";
+    private static Scanner input = new Scanner(System.in);
     public static void main(String args[])
     {
-        Game game = new Game();
-        game.startGame();
-        game.showHistory();
+        Game game = null;
+
+        System.out.println("Hello! It's Tic Tac Toe Game!");
+
+        boolean menuRun = true;
+        while(menuRun)
+        {
+            System.out.println("Please, select one of the menu items:");
+            System.out.println("1. Human vs Human");
+            System.out.println("2. Human vs Computer");
+            System.out.println("3. Show game history");
+            System.out.println("4. Exit");
+            System.out.print("I'm choosing: ");
+            if(input.hasNextInt())
+            {
+                switch(input.nextInt())
+                {
+                    case 1:
+                    {
+                        game = new Game(new HumanPlayer("Human1",'X'), new HumanPlayer("Human2", '0'), 3, 3);
+                        System.out.println("The Game started !");
+                        game.startGame();
+                        break;
+                    }
+                    case 2:
+                    {
+                        game = new Game(new HumanPlayer("Human1", 'X'), new ComputerPlayer("Computer1", ComplexityLevel.EASY, '0'), 3, 3);
+                        System.out.println("The Game started !");
+                        game.startGame();
+                        break;
+                    }
+                    case 3:
+                    {
+                        if(game != null)
+                        {
+                            game.showHistory();
+                        }
+                        else
+                        {
+                            System.out.println("No any history, maybe it's first game.");
+                        }
+                        break;
+                    }
+                    case 4:
+                    {
+                        menuRun = false;
+                        break;
+                    }
+                    default:
+                    {
+                        System.out.println(ERROR_MASSAGE);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                System.out.println(ERROR_MASSAGE);
+            }
+        }
     }
 }
